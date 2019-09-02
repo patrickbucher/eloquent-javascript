9630bc214dfd84981b58f5cc94d46a9ec66fbddd
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-02 13:17:10 +0200

    Chapter 4: minor corrections in exercises

--- exercises/ch04/sum-of-range.js
+++ exercises/ch04/sum-of-range.js
@@ -1,6 +1,7 @@
 console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10, 2)));
 
-function range(from, to, step=1) {
+function range(from, to, step = 1) {
 	let numbers = [];
 	for (let i = from; i <= to; i += step) {
 		numbers.push(i);
@@ -9,7 +10,7 @@ function range(from, to, step=1) {
 }
 
 function sum(numbers) {
-	sum = 0;
+	let sum = 0;
 	for (let x of numbers) {
 		sum += x;
 	}
