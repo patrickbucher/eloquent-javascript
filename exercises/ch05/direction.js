7ff378ba02de16c797c66281d3c14f4d216e60b0
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-08 10:47:11 +0200

    Chapter 5: dominant writing direction (refactoring, removed foreign code)

--- exercises/ch05/direction.js
+++ exercises/ch05/direction.js
@@ -1,30 +1,37 @@
+// TODO: get scripts.js from http://eloquentjavascript.net/code/scripts.js
 const scripts = require('./scripts')
 
 const text = 'ᠬᠦᠮᠦᠨ Я ничего не знаю.لحضور المؤتمر الدولي العاشر ليونيكود. Ja';
 
-// TODO: refactor using a more functional style
+const largestProperty = (obj) => {
+    let max = -Infinity, largest = '';
+    for (let key of Object.keys(obj)) {
+        if (obj[key] > max) {
+            max = obj[key];
+            largest = key;
+        }
+    }
+    return largest;
+}
+
+const withinRange = (value, lower, upper) => {
+    return value >= lower && value <= upper;
+}
+
 function dominantWritingDirection(text) {
     let directions = { 'ltr': 0, 'rtl': 0, 'ttb': 0};
-    for (let character of text) {
-        let code = character.codePointAt(0);
-        console.log(character, code);
-        for (let script of scripts) {
-            for (let [lower, upper] of script.ranges) {
-                if (code >= lower && code <= upper) {
+    Array.from(text).map(c => c.codePointAt(0)).forEach(code=> {
+        scripts.forEach(script => {
+            script.ranges.some(([lower, upper]) => {
+                if (withinRange(code, lower, upper)) {
                     directions[script.direction] += 1;
-                    break;
+                    return
                 }
-            }
-        }
-    }
-    let max = -1, dir = '';
-    for (let prop in directions) {
-        if (directions[prop] > max) {
-            max = directions[prop];
-            dir = prop;
-        }
-    }
-    return dir;
+            });
+        });
+    });
+    console.dir(directions);
+    return largestProperty(directions);
 }
 
 console.log(dominantWritingDirection(text));
